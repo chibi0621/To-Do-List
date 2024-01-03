7147b4e6891ffdb8ba839d00e37f84a3598abdb0
chibi0621 <cony12658@gmail.com>
2024-01-03 18:30:38 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# To-Do-List 
+用Javascript寫的待辦事項清單(to-do list)
+
+deployed on vercel ☞ 
+
+### - 取得專案
+`$ git clone `
+or 
+`$ git clone `
+
+### - 圖片
+
+
+### - 聯絡作者
+E-mail: chibishih1996@gmail.com
---
+++ js/all.js
@@ -0,0 +1,37 @@
+const content = document.getElementById('content');
+const time = document.getElementById('time');
+const addedBtn = document.getElementById('addedBtn');
+const deletedBtn = document.getElementById('deletedBtn');
+const list = document.getElementById('list');
+
+const listContent = []
+
+function render() {
+  let html5tr = ''
+    listContent.forEach(function (item) {
+      html5tr = html5tr + `
+        <div class="item">
+          <div>
+            <p>${item.time}要${item.content}</p>
+						<hr/>
+          </div>
+        </div>`
+    })
+    list.innerHTML = html5tr;
+}
+
+addedBtn.addEventListener('click' ,function () {
+  
+  listContent.push({
+    content: content.value,
+    time: time.value,
+  })
+
+  render();
+})
+
+deletedBtn.addEventListener('click',function () {
+	listContent.shift()
+
+  render();
+})
